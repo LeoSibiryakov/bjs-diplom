ecbe64066015d5dd08bbb08fa4d5cf1a02b6b2b6
Leonid Sibiryakov <leo-sibiryakov@yandex.ru>
2019-12-10 20:06:41 +0500

    making methods

--- static/main.js
+++ static/main.js
@@ -1,22 +1,66 @@
 "use strict"
 
 class profile {
-    constructor({username,name,password}) { // Или лучше сделать так - { username, name: { firstName, lastName }, password})
+    constructor({username,name: {firstName, lastName},password}) { 
         this.login = username;
-        this.name = name; // И соответственно так this.name = { firstName, lastName}; ?
+        this.name = {firstName, lastName};
         this.password = password;
     }
 
-    newUser(callback) {
-        // console.log(`Add ${this.login}`);  - можно без этого?
-        return ApiConnector.createUser({
+    addNewUser(callback) {
+        return ApiConnector.addNewUser({
             username: this.login,
             name: this.name,
             password: this.password
         },
-        (err,data) => {
+            (err,data) => {
             console.log(`Add user ${this.login}`);
-            callback(err,data); // Правильно ли выполен весь метод,или где-то нужно исправлять? 
-        }
-        ); // по такому же принципу делаются остальные методы? И затем пишется функция возврата.
+            callback(err,data);
+        });
     }
+
+    authorization(callback) {
+        return ApiConnector.authorization({
+            username: this.username,
+            password: this.password 
+        },
+            (err,data) => {
+            console.log(`Authorizing user ${this.username}`);
+            callback(err,data);
+        });
+    }
+
+    addMoney({currency, amount}, callback) {
+        return ApiConnector.addMoney({currency, amount}, 
+            (err, data) => {
+            console.log(`Added ${amount} of ${currency} to ${this.username}`);
+            callback(err, data);
+        });
+    }
+
+    convertation({ofCurrency,inCurrency,targetAmount}, callback) {
+        return ApiConnector.convertation({ofCurrency,inCurrency,targetAmount}),
+            (err,data) => {
+            console.log(`Converting ${ofCurrency} to ${targetAmount} ${inCurrency}`);
+            callback(err,data);
+        };
+    }
+
+    moneyTransfer({whom,amount},callback) {
+        return ApiConnector.moneyTransfer({whom,amount}),
+            (err,data) => {
+            console.log(`Transferring ${amount} to ${whom}`);
+            callback(err,data);
+        };
+    }
+}
+
+function getStocks(callback) {
+    return ApiConnector.getStocks((err,data) => {
+        console.log(`Getting stocks`);
+    });
+}
+
+/* Сделал методы по подобию примера в задании, посмотрите пожалуйста,все ли правильно?
+и по последней функции  - её нужно сохранить в переменную, типа let someName = getStocks и потом использовать где-то дальше?
+*/
